049aa2b00d712a47288afeeb5ef1b094630ffbb4
Justin Cassidy <boil.afraid@gmail.com>
2014-12-10 23:24:27 -0800

    fixed multiple js inserts from multiple tags

---
+++ htdocs/chess/head.js
@@ -0,0 +1,34 @@
+/* head.js: Moin Chess Plugin "stub" script
+ * December 2014
+ *
+ * Load this script with every {{{#!Chess}}} tag in the wiki. Upon first
+ * run, this script inserts the remaining CSS and JS code into the DOM to
+ * support nice-looking CSS chessboards and menus.
+ *
+ * For subsequent {{{#!Chess}}} tags in the page after the first one, we have
+ * to still insert this stub script, but inspection of the DOM will show that
+ * further inserts are unnecessary.
+ */
+
+// Change this every time the Wiki is upgraded
+var moin_chess_root = "/moin_static194/chess/"
+
+var style = document.createElement('link');
+style.rel = "stylesheet";
+style.href = moin_chess_root + "chess.css";
+
+var zeptojs = document.createElement('script');
+zeptojs.type = "text/javascript";
+zeptojs.src = moin_chess_root + "zepto.js";
+
+var behavior = document.createElement('script');
+behavior.type = "text/javascript";
+behavior.src = moin_chess_root + "chess.js";
+
+// Have we loaded the Chess JS? If so, don't load any of these 
+// resources a second time!
+if ( ! behavior.isEqualNode(document.head.lastChild)) {
+   document.head.appendChild(style);
+   document.head.appendChild(zeptojs);
+   document.head.appendChild(behavior);
+}
